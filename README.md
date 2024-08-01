3b767ee0d85f35508c5665b1a2ed3ef847b0775b
Akhmedov Yaqub <akhmedovya@humo.tj>
2024-08-01 10:30:12 +0500

    init

---
+++ README.md
@@ -0,0 +1,81 @@
+Certainly! Let's create a concise and informative `readme.md` for the **go-rabbitmq-client** utility in Go. Here you go:
+
+---
+
+# go-rabbitmq-client
+
+**go-rabbitmq-client** is a lightweight Go library for interacting with RabbitMQ. It provides features like auto-reconnect and panic handling, making it a reliable choice for RabbitMQ communication.
+
+## Features
+
+1. **Autoconnect**: The library automatically reconnects to the RabbitMQ server if the connection is lost. This ensures that your application remains resilient even in the face of network interruptions or RabbitMQ server restarts.
+
+2. **Panic Catcher**: go-rabbitmq-client includes a panic catcher mechanism. If an unexpected panic occurs during message processing, the library gracefully handles it, preventing your application from crashing. This is especially useful in long-running services.
+
+3. **Lightweight**: The library is designed to be minimalistic and efficient. It doesn't introduce unnecessary overhead, making it suitable for resource-constrained environments.
+
+## Installation
+
+To use go-rabbitmq-client in your Go project, simply import it:
+
+```go
+import "github.com/Jacobamv/go-rabbitmq-client"
+```
+
+Make sure to replace `Jacobamv` with your actual GitHub username or the appropriate module path.
+
+## Usage
+
+Here's a basic example of how to use go-rabbitmq-client:
+
+```go
+package main
+
+import (
+	"fmt"
+	rabbitmq "https://github.com/Jacobamv/go-rabbitmq-client"
+)
+
+func Test(msg []byte) error {
+	fmt.Println("got a message", string(msg))
+
+	return nil
+}
+
+func main() {
+
+	rmqp := fmt.Sprintf("amqp://%v:%v@%v:%v/",
+		"guest",
+		"guest",
+		"localhost",
+		"5672",
+	)
+
+	client, err := rabbitmq.NewClient(rmqp)
+
+	if err != nil {
+		return
+	}
+
+	client.Consume("test", Test)
+
+	client.Run()
+}
+
+```
+
+Remember to adjust the connection URL (`"amqp://guest:guest@localhost:5672/"`) and other settings according to your RabbitMQ setup.
+
+## Contributing
+
+Contributions are welcome! Feel free to open issues or submit pull requests on the [GitHub repository](https://github.com/Jacobamv/go-rabbitmq-client).
+
+## License
+
+This project is licensed under the GNU 3 LICENSE. See the [LICENSE](LICENSE) file for details.
+
+---
+
+
+: [RabbitMQ Official Documentation](https://www.rabbitmq.com/documentation.html)
+: [go-rabbitmq-client GitHub Repository](https://github.com/Jacobamv/go-rabbitmq-client)
---
+++ client.go
@@ -0,0 +1,199 @@
+package rabbitmq
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type provider struct {
+	Address string
+
+	Connection *amqp.Connection
+	Channel    *amqp.Channel
+
+	Consumers map[string]func([]byte) error
+}
+
+type Client interface {
+	Consume(queueName string, consumer func([]byte) error)
+	Run()
+}
+
+type Connection interface {
+	Close() error
+}
+
+func (p *provider) NewChannel() (err error) {
+	p.Channel, err = p.Connection.Channel()
+
+	if err != nil {
+		return err
+	}
+
+	//go p.AutoConnect(p.Channel.NotifyClose(make(chan *amqp.Error)), p.Channel)
+
+	return nil
+}
+
+func (p *provider) Connect(rmqpAddress string) error {
+	conn, err := amqp.Dial(rmqpAddress)
+	if err != nil {
+		return err
+	}
+
+	p.Address = rmqpAddress
+	p.Connection = conn
+	conn.Config.Heartbeat = 15 * time.Minute
+
+	go p.AutoConnect(p.Connection.NotifyClose(make(chan *amqp.Error)), p.Connection)
+
+	return nil
+}
+
+func (p *provider) AutoConnect(notifyClose chan *amqp.Error, object Connection) {
+	var err error
+	defer object.Close()
+
+	for {
+		select {
+		case reason, ok := <-notifyClose:
+			if !ok {
+				fmt.Println("Connection closed by the developer")
+				return
+			}
+
+			fmt.Println("RabbitMQ connection closed", reason.Reason)
+
+			// Attempt to reconnect
+			for {
+				select {
+				case <-context.Background().Done():
+					fmt.Println("Context canceled, stopping reconnect attempts")
+					return
+				default:
+					fmt.Println("Attempting to reconnect to RabbitMQ")
+
+					err = p.Connect(p.Address)
+
+					err = p.NewChannel()
+					if err == nil {
+						fmt.Println("Reconnected successfully to RabbitMQ")
+						p.Run()
+
+						return
+					}
+
+					fmt.Println("Reconnect failed, retrying")
+					time.Sleep(time.Second * 3)
+				}
+			}
+
+		case <-context.Background().Done():
+			fmt.Println("Context canceled, stopping reconnect attempts")
+			return
+		}
+	}
+}
+
+func NewClient(rmqpAddress string) (Client, error) {
+	client := &provider{
+		Consumers: make(map[string]func([]byte) error, 0),
+	}
+	var err error
+
+	err = client.Connect(rmqpAddress)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.NewChannel()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func (p *provider) Consume(queueName string, consumer func([]byte) error) {
+	p.Consumers[queueName] = consumer
+}
+
+func (p *provider) Run() {
+
+	for queueName, consumerFunc := range p.Consumers {
+		q, err := p.Channel.QueueDeclare(
+			queueName, // name
+			false,     // durable
+			false,     // delete when unused
+			false,     // exclusive
+			false,     // no-wait
+			nil,       // arguments
+		)
+		if err != nil {
+			log.Fatal("Failed to declare a queue")
+		}
+
+		msgs, err := p.Channel.Consume(
+			q.Name, // queue
+			"",     // consumer
+			false,  // auto-ack (set to false for manual acknowledgment)
+			false,  // exclusive
+			false,  // no-local
+			false,  // no-wait
+			nil,    // args
+		)
+		if err != nil {
+			log.Fatal("Failed to register a consumer")
+		}
+
+		fmt.Println("waiting for", queueName)
+
+		go func(queueName string, consumerFunc func([]byte) error) {
+			for msg := range msgs {
+				if err := processMessage(p.Channel, msg, consumerFunc); err != nil {
+					if err := republishMessage(p.Channel, msg, queueName); err != nil {
+						log.Printf("Failed to republish message from queue %s", queueName)
+					}
+				}
+			}
+		}(queueName, consumerFunc)
+	}
+
+	// Keep the application running to listen for messages
+	select {}
+}
+
+func processMessage(ch *amqp.Channel, msg amqp.Delivery, consumerFunc func([]byte) error) error {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Printf("Recovered in processMessage: %v", r)
+		}
+	}()
+
+	err := consumerFunc(msg.Body)
+
+	if err != nil {
+		return err
+	} else {
+		return ch.Ack(msg.DeliveryTag, false)
+	}
+}
+
+func republishMessage(ch *amqp.Channel, msg amqp.Delivery, queueName string) error {
+
+	return ch.PublishWithContext(context.Background(),
+		"",        // exchange
+		queueName, // routing key
+		false,     // mandatory
+		false,     // immediate
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        msg.Body,
+		})
+}
